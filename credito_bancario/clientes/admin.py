ec11a5817679dbb66cb0aea1e2a861457af1ee5a
Paloma Cezário <147927123+PalomaCezario@users.noreply.github.com>
2024-11-12 19:19:57 -0300

    Implementação das operações CRUD para o modelo Cliente com campos de escolha

--- credito_bancario/clientes/admin.py
+++ credito_bancario/clientes/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib import admin # type: ignore
+from .models import Cliente
 
-# Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('sexo', 'regiao', 'estado_civil', 'numero_filhos', 'propriedade', 'educacao', 'vinculo_empregaticio', 'fonte_renda', 'marker')
+    list_filter = ('sexo', 'regiao', 'estado_civil', 'numero_filhos', 'propriedade', 'educacao', 'vinculo_empregaticio', 'fonte_renda', 'marker')
+    search_fields = ('sexo', 'regiao', 'estado_civil')
+
